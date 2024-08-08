464d97f465f9fea931a30a8951cee9ac1d8075b5
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2024-08-08 12:23:54 +0530

    a = int(input()) b = int(input()) print("Sum:",a+b, sep="") print("Difference:",a - b, sep="") print("Product:",a * b, sep="") print("Quotient:",int(a / b), sep="") print("Remainder:",a % b, sep="")

---
+++ Arithmetic_operations.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+print("Sum:",a+b, sep="")
+print("Difference:",a - b, sep="")
+print("Product:",a * b, sep="")
+print("Quotient:",int(a / b), sep="")
+print("Remainder:",a % b, sep="")
